d12ad1bd1994b9211cb3d7d03894b60d0ab4d508
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-11-25 14:11:40 +0900

    [CSE-18] 엔티티와 레포 작성

---
+++ modules/draw/draw-impl/src/main/java/com/eodi/bium/draw/entity/DrawPoint.java
@@ -0,0 +1,44 @@
+package com.eodi.bium.draw.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "draw_point") // 테이블 이름 예시
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DrawPoint {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "draw_point_id")
+    private Long id;
+
+    @Column(name = "record_id", nullable = false)
+    private Long recordId;
+
+    @Column(name = "event_id", nullable = false)
+    private Long eventId;
+
+    @Column(name = "member_id", nullable = false)
+    private String memberId;
+
+    @Column(name = "point", nullable = false)
+    private Integer point;
+
+    @Builder
+    public DrawPoint(Long eventId, String memberId, Integer point, Long recordId) {
+        this.recordId = recordId;
+        this.eventId = eventId;
+        this.memberId = memberId;
+        this.point = point;
+    }
+}
---
+++ modules/draw/draw-impl/src/test/java/com/eodi/bium/DrawTest.java
@@ -0,0 +1,27 @@
+package com.eodi.bium;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class DrawTest {
+
+    @Test
+    public void 추첨을_시작한다() {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    public void 추첨에_참여한다() {
+        //given
+
+        //when
+
+        //then
+    }
+}
