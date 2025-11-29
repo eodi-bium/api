d4d1febbf1dd747ea54bd045ad7d8f1548596aef
Oh YoungJe <139232765+GulSauce@users.noreply.github.com>
2025-11-29 23:56:36 +0900

    [CSE-32] 구현 완료

---
+++ modules/member/member-impl/src/main/java/com/eodi/bium/member/component/Redis.java
@@ -0,0 +1,20 @@
+package com.eodi.bium.member.component;
+
+import com.eodi.bium.member.properties.RedisProperties;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+@AllArgsConstructor
+public class Redis {
+
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisProperties.getRedisConfiguration());
+    }
+}
