83605e44f1bf6189247c69e6b4554e717b2c8e81
lhoju0158 <lhoju0158@gmail.com>
2025-11-30 22:36:46 +0900

    [CSE-34] refactor: 오류 수정

--- modules/member/member-impl/src/main/java/com/eodi/bium/member/controller/MemberInfoController.java
+++ modules/member/member-impl/src/main/java/com/eodi/bium/member/controller/MemberInfoController.java
@@ -16,19 +16,8 @@ public class MemberInfoController {
     private final MemberInfoService memberInfoService;
 
     @GetMapping("/memberInfo")
-    public ResponseEntity<?> getInfo() {
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-        System.out.println("principal = " + principal);
-        String memberId = "";
-
-        // 2. Principal이 UserDetails 타입인지 확인 후 형변환
-        if (principal instanceof UserDetails) {
-            memberId = ((UserDetails) principal).getUsername(); // UserDetails의 username이 memberId입니다.
-        } else {
-            // principal이 문자열("anonymousUser")인 경우 등 예외 처리
-            memberId = principal.toString();
-        }
+    public ResponseEntity<?> getMemberInfo(@AuthenticationPrincipal UserDetails userDetails) {
+        String memberId = userDetails.getUsername();
         return ResponseEntity.ok(memberInfoService.getMemberInfo(memberId));
     }
 }
